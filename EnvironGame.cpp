709126bd7054cac263834b512451f3dbbe64c19c
Jorge Ayala <jayala42000@gmail.com>
2024-09-12 22:50:36 -0500

    v1 - project initialization

--- EnvironGame.cpp
+++ EnvironGame.cpp
@@ -4,9 +4,12 @@ int main(){
     // Reinicializar valores aleatorios
     srand(time(NULL));
     // Cambiar tamaño de la consola y centrarla
-    resizeConsole(1200, 650);
+    resizeConsole(1000, 500);
 
-    cout << "|| Inicializacion del proyecto V1 !!" << endl;
+    // Empecemos...
+    cout << "=====================================" << endl;
+    cout << "=> Empecemos con el proyecto :D !! <=" << endl;
+    cout << "=====================================" << endl;
 
     return 0;
 }
